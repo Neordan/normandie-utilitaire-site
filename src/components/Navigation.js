5b39b4d2a967c44d684fc4dbcdb9c6ad266fe0a7
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-07 23:56:12 +0200

    Création du component navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from "react-router-dom"
+
+const Navigation = () => {
+    return (
+        <div>
+            <div className="navigation">
+                <NavLink></NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
