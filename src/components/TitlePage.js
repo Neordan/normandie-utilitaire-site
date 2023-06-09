59ed8246cc6bd33755f3e7553a03f03c36bf3ab4
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-09 20:33:06 +0200

    Ajout de la props image pour l'ajout d'image autour du titre

--- src/components/TitlePage.js
+++ src/components/TitlePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const TitlePage = ({ title }) => {
+const TitlePage = ({ title , image}) => {
     return (
         <div className="title">
+            <img src={require(image)} alt="" />
             <h2>{title}</h2>
         </div>
     );
