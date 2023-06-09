314d99983be7ba01147ac0bd17be881a6b6baa10
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-09 19:47:43 +0200

    Ajout du copyright et du logo dans le footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className="footer">
-            <NavLink NavLink to="/contact" className={(nav) => (nav.isActive ? "nav-active-footer" : "")}>
+            <div className="footer-link">
+                <NavLink NavLink to="/contact" className={(nav) => (nav.isActive ? "nav-active-footer" : "")}>
                     <li>Contactez-nous</li>
                 </NavLink>
                 <NavLink to="/mentions_legales" className={(nav) => (nav.isActive ? "nav-active-footer" : "")}>
@@ -13,6 +14,11 @@ const Footer = () => {
                 <NavLink to="/politique_de_confidentialite" className={(nav) => (nav.isActive ? "nav-active-footer" : "")}>
                     <li>Politique de confidentialité</li>
                 </NavLink>
+            </div>
+            <img src={require("../assets/img/logoversionmobile.png")} alt="Logo de Normandie Utilitaire" />
+            <p>
+                © 2023 Normandie Utilitaire. Tous droits réservés.
+            </p>
         </div>
     );
 };
