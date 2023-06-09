8cf73cf64bc246383d08507da3796755166f4e64
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-09 02:33:20 +0200

    Création du block du titre principal

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Description = () => {
     return (
         <div className="description">
-            
+            <div className="title-container">
+                <h1>Découvrez Normandie Utilisataire :</h1>
+                <p>votre partenaire incontournable pour des services de qualité et une expérience client exceptionnelle.</p>
+            </div>
         </div>
     );
 };
