b82b28c87ffef979029af1aaefe018510c8acfb6
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-09 19:52:54 +0200

    Ajout des components logo, navigation et footer

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+
+const Contact = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <Footer />
+        </div>
+    );
+};
+
+export default Contact;
