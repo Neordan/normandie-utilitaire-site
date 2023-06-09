7bb3616c87d459165785e2ac680d36752c341dd3
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-09 02:00:57 +0200

    Ajout des routes pour l'url dans l'App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
+import Repair from './pages/Repair';
+import Location from './pages/Location';
+import LegalNotice from './pages/LegalNotice';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+
 
 const App = () => {
     return (
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/reparation" element={<Repair />}/>
+            <Route path="/location" element={<Location />}/>
+            <Route path="/mentions_legales" element={<LegalNotice />}/>
+            <Route path="/politique_de_confidentialite" element={<PrivacyPolicy />}/>
             <Route path="*" element={<Home/>}/>
         </Routes>
         </BrowserRouter>
