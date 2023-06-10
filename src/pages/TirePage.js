5cb9b1197a9f0d437a62f3c478080cce5b96df89
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-10 21:24:21 +0200

    Ajout des components pour la page des pneus et insertion du formulaire

--- src/pages/TirePage.js
+++ src/pages/TirePage.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+import TitlePage from '../components/TitlePage';
+import Footer from '../components/Footer';
 
 const TirePage = () => {
     return (
         <div>
-            
+            <Logo />
+            <Navigation />
+            <TitlePage title="réparation" image={require('../assets/img/icons/pneu (1).png')} alt="Icône de pneus" />
+            <form action="" method="post">
+                <label htmlFor="width">Largeur</label>
+                <select name="width" id=""></select>
+                <label htmlFor="height">Hauteur</label>
+                <select name="height" id=""></select>
+                <label htmlFor="diameter">Diamètre</label>
+                <select name="diameter" id=""></select>
+                <label htmlFor="charge">Indice de charge</label>
+                <select name="charge" id=""></select>
+                <label htmlFor="speed">Indice de vitesse</label>
+                <select name="speed" id=""></select>
+                <button className="validate">valider</button>
+            </form>
+            <Footer />
         </div>
     );
 };
