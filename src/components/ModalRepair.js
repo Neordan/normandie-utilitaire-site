70ca5109d29f47aae68d4e6c1d24628105f30386
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-11 01:18:16 +0200

    Création de la modal de confirmation pour les réparations

--- src/components/ModalRepair.js
+++ src/components/ModalRepair.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ModalRepair = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [appointmentDate, setAppointmentDate] = useState('');
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <div>
-            
+            <h1>Confirmation</h1>
+            <form onSubmit={handleFormSubmit}>
+                <div>
+                    <label htmlFor="firstName">First Name:</label>
+                    <input
+                        type="text"
+                        id="firstName"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="lastName">Last Name:</label>
+                    <input
+                        type="text"
+                        id="lastName"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="phone">Phone:</label>
+                    <input
+                        type="tel"
+                        id="phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="appointmentDate">Appointment Date:</label>
+                    <input
+                        type="date"
+                        id="appointmentDate"
+                        value={appointmentDate}
+                        onChange={(e) => setAppointmentDate(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Validate</button>
+            </form>
         </div>
     );
 };
