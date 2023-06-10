c2b8a4eeddb27b3561e59c5adb94272f74a3c0b4
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-10 20:28:03 +0200

    Création des 3 pages pour les réparations

---
+++ src/pages/MechanicsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MechanicsPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MechanicsPage;
---
+++ src/pages/TirePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TirePage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TirePage;
