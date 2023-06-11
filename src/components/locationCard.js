8aa2ff3fe91946face01c10f3e667bac6660133d
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-11 23:23:33 +0200

    Ajout du lien pour la card des camions

--- src/components/locationCard.js
+++ src/components/locationCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const LocationCard = ({ truckType, imageUrl, dailyRate, weeklyRate }) => {
+const LocationCard = ({ truckType, imageUrl, link }) => {
     return (
         // <Link to={`/camion/${camionId}`}>
-            <div className="location-card">
+            <a href={link}className="location-card">
                 <img src={imageUrl} alt={truckType} />
                 <div className="location-under-card">
                     <p>{truckType}</p>
                     <p>Tarif à la journée / à la semaine</p>
                 </div>
-            </div>
+            </a>
         // </Link>
     );
 };
