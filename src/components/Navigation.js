caa7c67824ed1a33e254df24165158082fbe61c6
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-12 01:00:17 +0200

    Ajout de ma classe nav pour les onglets de menu non actifs

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,13 +5,13 @@ const Navigation = () => {
     return (
         <div>
             <div className="navigation">
-                <NavLink NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+                <NavLink NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "nav")}>
                     <li>Accueil</li>
                 </NavLink>
-                <NavLink to="/reparation" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+                <NavLink to="/reparation" className={(nav) => (nav.isActive ? "nav-active" : "nav")}>
                     <li>Réparation</li>
                 </NavLink>
-                <NavLink to="/location" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+                <NavLink to="/location" className={(nav) => (nav.isActive ? "nav-active" : "nav")}>
                     <li>Location</li>
                 </NavLink>
             </div>
