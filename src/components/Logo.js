6b03fd52f1b8d913e1834b31d35198da3de7224b
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-11 23:58:36 +0200

    Ajout du lien dans le component logo pour l'accueil

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Logo = () => {
     return (
-        <div className="logo">
+        <a href="/" className="logo">
             <img src={require('../assets/img/logoversionmobile.png')} alt="Logo de Normandie Utilitaire" />
-        </div>
+        </a>
     );
 };
 
