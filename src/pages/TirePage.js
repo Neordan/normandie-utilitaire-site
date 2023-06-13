8656ff3173f9b9a2df97ab1db5188e575c8e0f31
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-13 23:34:11 +0200

    Création de la page de pneu

--- src/pages/TirePage.js
+++ src/pages/TirePage.js
@@ -10,16 +10,16 @@ const TirePage = () => {
             <Logo />
             <Navigation />
             <TitlePage title="Pneumatiques" image={require('../assets/img/icons/pneu (1).png')} alt="Icône de pneus" />
-            <form action="" method="post">
-                <label htmlFor="width">Largeur</label>
+            <form className="tire-form" action="" method="post">
+                <label htmlFor="width">Largeur :</label>
                 <select name="width" id=""></select>
-                <label htmlFor="height">Hauteur</label>
+                <label htmlFor="height">Hauteur :</label>
                 <select name="height" id=""></select>
-                <label htmlFor="diameter">Diamètre</label>
+                <label htmlFor="diameter">Diamètre :</label>
                 <select name="diameter" id=""></select>
-                <label htmlFor="charge">Indice de charge</label>
+                <label htmlFor="charge">Indice de charge :</label>
                 <select name="charge" id=""></select>
-                <label htmlFor="speed">Indice de vitesse</label>
+                <label htmlFor="speed">Indice de vitesse :</label>
                 <select name="speed" id=""></select>
                 <button className="validate">valider</button>
             </form>
