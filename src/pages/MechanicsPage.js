b86363612308057ca88e32a79bd334815fce3c3b
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-19 22:12:55 +0200

    Rectification de la page de mécanique

--- src/pages/MechanicsPage.js
+++ src/pages/MechanicsPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CheckboxInput from '../components/CheckboxInput';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
@@ -7,6 +7,7 @@ import Footer from '../components/Footer';
 import ModalRepair from '../components/ModalRepair';
 
 const MechanicsPage = () => {
+  const [showModal, setShowModal] = useState(false);
   const [formState, setFormState] = useState({
     freinage: false,
     batterie: false,
@@ -19,13 +20,48 @@ const MechanicsPage = () => {
     carrosserie: false
   });
 
-  const [showModal, setShowModal] = useState(false);
-  const [selectedMechanicsOptions, setSelectedMechanicsOptions] = useState([]);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [appointmentDate, setAppointmentDate] = useState('');
+  const [selectedMechanicsOptions, setSelectedMechanicsOptions] = useState({});
+  const [mechanicsOptions, setMechanicsOptions] = useState([]);
+
+  useEffect(() => {
+    const storedOptions = JSON.parse(localStorage.getItem('selectedMechanicsOptions')) || {};
+    setFormState(storedOptions);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('selectedMechanicsOptions', JSON.stringify(selectedMechanicsOptions));
+  }, [selectedMechanicsOptions]);
+
+  useEffect(() => {
+    const mechanicsOptions = [];
+
+    if (selectedMechanicsOptions['freinage']) {
+      mechanicsOptions.push('Freinage');
+    }
+
+    // Ajoutez les autres options ici avec une structure similaire
+
+    setMechanicsOptions(mechanicsOptions);
+  }, [selectedMechanicsOptions]);
+
+
+  const toggleOption = (option) => {
+    setSelectedMechanicsOptions((prevOptions) => {
+      const updatedOptions = {
+        ...prevOptions,
+        [option]: !prevOptions[option],
+      };
+      return updatedOptions;
+    });
+  
+    setMechanicsOptions((prevOptions) => {
+      if (prevOptions.includes(option)) {
+        return prevOptions.filter((item) => item !== option);
+      } else {
+        return [...prevOptions, option];
+      }
+    });
+  };
 
   const handleInputChange = (id, checked) => {
     setFormState((prevState) => ({
@@ -35,138 +71,125 @@ const MechanicsPage = () => {
   };
 
   const handleContinueClick = () => {
-    const selectedOptions = Object.keys(formState).filter((key) => formState[key]);
-    setSelectedMechanicsOptions(selectedOptions);
-    setShowModal(true);
-  };
+    const selectedMechanicsOptions = Object.keys(formState).filter((key) => formState[key]);
+    console.log('Selected mechanics options:', mechanicsOptions);
+};
 
-  const handleFormSubmit = (event) => {
+const handleFormSubmit = (event) => {
     event.preventDefault();
-    // Gérer la soumission du formulaire ici (envoi de données, etc.)
+    setShowModal(true);
   };
 
   const closeModal = () => {
     setShowModal(false);
   };
 
+  console.log(mechanicsOptions);
 
-    return (
-        <div>
-            <Logo />
-            <Navigation />
-            <TitlePage
-                title="Mécanique & entretien"
-                image={require('../assets/img/icons/outils.png')}
-                alt="Icône d'outils"
-            />
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <TitlePage
+        title="Mécanique & entretien"
+        image={require('../assets/img/icons/outils.png')}
+        alt="Icône d'outils"
+      />
 
-{showModal && (
+      {showModal && (
         <ModalRepair
-          selectedOptions={selectedMechanicsOptions}
-          toggleOption={() => setShowModal(!showModal)}
+          selectedOptions={['mechanics']}
+          selectedMechanicsOptions={mechanicsOptions}
           closeModal={closeModal}
-          firstName={firstName}
-          setFirstName={setFirstName}
-          lastName={lastName}
-          setLastName={setLastName}
-          email={email}
-          setEmail={setEmail}
-          phone={phone}
-          setPhone={setPhone}
-          appointmentDate={appointmentDate}
-          setAppointmentDate={setAppointmentDate}
         />
       )}
-      
-            <form className="mechanics-form" onSubmit={handleFormSubmit}>
-                <div className="form-group">
-                    <CheckboxInput
-                        id="freinage"
-                        label="Freinage"
-                        checked={formState.freinage}
-                        onChange={handleInputChange}
-                    />
 
-                    <CheckboxInput
-                        id="batterie"
-                        label={
-                            <div className="label">
-                                <span>Batterie</span>
-                                <span className="separator" />
-                                <span>Démarrage</span>
-                            </div>
-                        }
-                        checked={formState.batterie}
-                        onChange={handleInputChange}
-                    />
-
-                    <CheckboxInput
-                        id="suspension"
-                        label={
-                            <div className="label">
-                                <span>Direction</span>
-                                <span className="separator" />
-                                <span>Suspension</span>
-                            </div>
-                        }
-                        checked={formState.suspension}
-                        onChange={handleInputChange}
-                    />
-                    <CheckboxInput
-                        id="courroie"
-                        label="Courroie de distribution"
-                        checked={formState.courroie}
-                        onChange={handleInputChange}
-                    />
-                    <CheckboxInput
-                        id="transmission"
-                        label={
-                            <div className="label">
-                                <span>Transmission</span>
-                                <span className="separator" />
-                                <span>Embrayage</span>
-                            </div>
-                        }
-                        alt="Photo d'un levier de vitesse"
-                        checked={formState.transmission}
-                        onChange={handleInputChange}
-                    />
-                    <CheckboxInput
-                        id="echappement"
-                        label="Échappement"
-                        alt="Photo d'un échappement"
-                        checked={formState.echappement}
-                        onChange={handleInputChange}
-                    />
-                    <CheckboxInput
-                        id="moteur"
-                        label="Motorisation"
-                        alt="Photo d'un moteur"
-                        checked={formState.moteur}
-                        onChange={handleInputChange}
-                    />
-                    <CheckboxInput
-                        id="eclairage"
-                        label="Éclairage"
-                        checked={formState.eclairage}
-                        onChange={handleInputChange}
-                    />
-                    <CheckboxInput
-                        id="carrosserie"
-                        label="Carrosserie"
-                        alt="Photo d'un homme s'occupant de la carroserie d'une voiture"
-                        checked={formState.carrosserie}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <button className="validate" onClick={handleContinueClick}>
-                    Valider
-                </button>
-            </form>
-            <Footer />
+      <form className="mechanics-form" onSubmit={handleFormSubmit}>
+        <div className="form-group">
+          <CheckboxInput
+            id="freinage"
+            label="Freinage"
+            checked={formState.freinage}
+            onChange={handleInputChange}
+          />
+          <CheckboxInput
+            id="batterie"
+            label={
+              <div className="label">
+                <span>Batterie</span>
+                <span className="separator" />
+                <span>Démarrage</span>
+              </div>
+            }
+            checked={formState.batterie}
+            onChange={handleInputChange}
+          />
+          <CheckboxInput
+            id="suspension"
+            label={
+              <div className="label">
+                <span>Direction</span>
+                <span className="separator" />
+                <span>Suspension</span>
+              </div>
+            }
+            checked={formState.suspension}
+            onChange={handleInputChange}
+          />
+          <CheckboxInput
+            id="courroie"
+            label="Courroie de distribution"
+            checked={formState.courroie}
+            onChange={handleInputChange}
+          />
+          <CheckboxInput
+            id="transmission"
+            label={
+              <div className="label">
+                <span>Transmission</span>
+                <span className="separator" />
+                <span>Embrayage</span>
+              </div>
+            }
+            alt="Photo d'un levier de vitesse"
+            checked={formState.transmission}
+            onChange={handleInputChange}
+          />
+          <CheckboxInput
+            id="echappement"
+            label="Échappement"
+            alt="Photo d'un échappement"
+            checked={formState.echappement}
+            onChange={handleInputChange}
+          />
+          <CheckboxInput
+            id="moteur"
+            label="Motorisation"
+            alt="Photo d'un moteur"
+            checked={formState.moteur}
+            onChange={handleInputChange}
+          />
+          <CheckboxInput
+            id="eclairage"
+            label="Éclairage"
+            checked={formState.eclairage}
+            onChange={handleInputChange}
+          />
+          <CheckboxInput
+            id="carrosserie"
+            label="Carrosserie"
+            alt="Photo d'un homme s'occupant de la carrosserie d'une voiture"
+            checked={formState.carrosserie}
+            onChange={handleInputChange}
+          />
         </div>
-    );
+        <button className="validate">
+          Valider
+        </button>
+      </form>
+      <Footer />
+    </div>
+  );
 };
 
 export default MechanicsPage;
-
